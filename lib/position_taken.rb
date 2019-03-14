e57887524728a61f8739cfc4792cae0b1012ddda
Robert <r_lee94@yahoo.com>
2019-03-14 18:15:17 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,8 +2,8 @@ def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || 
     board[index] == nil
     
-return false 
-else true 
+  return false 
+  else true 
 end
   
 end
